58ea29e8f8ff8a5d038401ebf6f280911742895c
Mohammed Almadhoon <16567+malmadhoo@noreply.learn.reboot01.com>
2026-05-24 16:25:17 +0000

    fixed player

--- js/core/entities/player.js
+++ js/core/entities/player.js
@@ -1,12 +1,69 @@
-export function createPlayer(spawnX = 0, spawnY = 0, playerSpeed = 5) {
-    const element = document.createElement('div');
+import { animationData, animationState, positions } from "../../data/data.js";
+import { entities } from "../../data/entities.js";
+import { input } from "../systems/input.js";
 
-    element.classList.add('player');
+export function createPlayer(
+    playerHeight = 32,
+    playerWidth = 32,
+    playerSpeed = 100,
+    spawnY = 0,
+    spawnX = 0,
+    spriteSheet = ""
+) {
+    const element = document.createElement("div");
 
-    return {
-        element,
-        x : spawnX,
-        y : spawnY, 
-        speed : playerSpeed
+    element.style.backgroundImage = `url(${spriteSheet})`;
+    element.style.transform = `translate(${spawnX}px, ${spawnY}px)`;
+    element.style.height = `${playerHeight}px`;
+    element.style.width = `${playerWidth}px`;
+    element.style.position = "absolute";
+    element.style.backgroundRepeat = "no-repeat";
+    element.style.imageRendering = "pixelated";
+    element.style.overflow = "hidden";
+    element.style.backgroundSize = "auto";
+    element.style.willChange = "transform";
+    element.id = "player";
+
+    positions["player"] = {
+        x: spawnX,
+        y: spawnY
     }
+    return element;
+}
+
+export function updatePlayer() {
+
+    let direction = 1;
+
+    if (input.left) {
+
+        animationState["player"].spriteName = "run";
+        direction = -1;
+
+    } else if (input.right) {
+
+        animationState["player"].spriteName = "run";
+
+    } else {
+        animationState["player"].spriteName = "idle";
+    }
+
+    const player = entities["player"];
+
+    const width = parseInt(player.style.width);
+    const height = parseInt(player.style.height);
+
+    const state = animationState["player"];
+    const anim = animationData["player"][state.spriteName];
+
+    const spriteX = width * (anim.startFrame + state.frame);
+    const spriteY = height * anim.row;
+
+    player.style.backgroundPosition =
+        `-${spriteX}px -${spriteY}px`;
+
+
+    // Render player
+    player.style.transform =
+        `translate(${positions["player"].x}px, ${positions["player"].y}px) scaleX(${direction})`;
 }
