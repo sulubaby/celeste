09da8f0c8784fcf9cc83e4b4143b057dae1f5e07
Mohammed Abbas <mohammedabbas.3640@gmail.com>
2026-06-22 18:33:34 +0300

    pause menu

--- js/systems/input.js
+++ js/systems/input.js
@@ -1,30 +1,36 @@
 export let inputs = [];
 export const keys = {
-    right: 'ArrowRight',
-    left: 'ArrowLeft',
-    jump: 'space',
-    dash: 'w',
-}
+  right: "ArrowRight",
+  left: "ArrowLeft",
+  jump: "space",
+  dash: "w",
+  pause: "e",
+};
 
-export function initInputs(rightKey = 'ArrowRight', leftKey = 'ArrowLeft', JumpKey = 'ArrowUp', DashKey = 'w') {
-    keys.left = leftKey;
-    keys.right = rightKey;
-    keys.jump = JumpKey;
-    keys.dash = DashKey;
+export function initInputs(
+  rightKey = "ArrowRight",
+  leftKey = "ArrowLeft",
+  JumpKey = "ArrowUp",
+  DashKey = "w",
+) {
+  keys.left = leftKey;
+  keys.right = rightKey;
+  keys.jump = JumpKey;
+  keys.dash = DashKey;
 
-    window.addEventListener('keydown', detectInput);
-    window.addEventListener('keyup', removeKey);
+  window.addEventListener("keydown", detectInput);
+  window.addEventListener("keyup", removeKey);
 }
 
 export function detectInput(e) {
-    if (!inputs.includes(e.key)) {
-        inputs.push(e.key);
-    }
+  if (!inputs.includes(e.key)) {
+    inputs.push(e.key);
+  }
 }
 
 export function removeKey(e) {
-    const index = inputs.indexOf(e.key);
-    if (index !== -1) {
-        inputs.splice(index, 1);
-    }
+  const index = inputs.indexOf(e.key);
+  if (index !== -1) {
+    inputs.splice(index, 1);
+  }
 }
